23ad8606d9827627fcaa3a3d1a2a0e4cb1bb033f
Tusar <tusarahmed141@gmail.com>
2022-06-01 10:32:53 +0600

    change background v - 1.1

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,21 +1,46 @@
+/*
+* Project requirement
+* + Change the background color by generating random RGB color
+* + Change the background color by generating random hex color
+* + Also display the hex code to a displayed input filled
+*/
+
 window.onload = () => {
   main()
 }
 
 const main = () => {
+  // Collect all references
   const container = document.getElementById('container');
   const cardBtn = document.querySelector('.card__btn');
+  const cardInp = document.querySelector('.card__input');
+
+  // Click event handler
   cardBtn.addEventListener('click', e => {
-    const generatedBG = randomRGB();
+    const generatedBG = randomHEX();
     container.style.backgroundColor = generatedBG;
+    cardInp.value = generatedBG;
   })
-  return container.style.backgroundColor = randomRGB();
+  
+    container.style.backgroundColor = randomHEX();
+    cardInp.value = randomHEX();
 }
 
+// Generate Random color
+/*
 const randomRGB = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
 
   return `rgb(${red}, ${green}, ${blue})`;
+}
+*/
+
+const randomHEX = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
